441aca35ea21e00cc20eb8d9be50a226fdb32db1
Sean Tauber <seanetauber@gmail.com>
2014-06-13 12:58:28 +0930

    created a web dashboard for viewing psycloud experiments in real-time

---
+++ dashboard/dashboard.py
@@ -0,0 +1,76 @@
+import os
+import json
+
+
+from psycloudclient import PsycloudClient, PsycloudAdminClient
+psycloud_client = PsycloudClient("http://psycloud-server-1.appspot.com")
+psycloud_aclient = PsycloudAdminClient("http://psycloud-server-1.appspot.com", "username", "password")
+
+# Import the Flask Framework
+from flask import Flask, render_template, request, redirect, url_for, abort, jsonify
+from flask.ext.httpauth import HTTPBasicAuth
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@app.route('/', methods=['GET'])
+@auth.login_required
+def main_dashboard():
+	exp_list = psycloud_aclient.get_experiment_list()['result']['experiments']
+	exps = []
+	for exp in exp_list:
+		exps.append({'name':exp['experiment_name'], 'id':exp['id'],
+			'num_available':len(exp['available_participants']),
+			'num_active':len(exp['active_participants']),
+			'num_completed':len(exp['completed_participants']),
+			'num_participants':exp['num_participants']})
+	return render_template('main_dashboard.html', params=exps)
+
+@app.route('/experiment/<exp_id>', methods=['GET'])
+@auth.login_required
+def view_experiment(exp_id):
+	exp = psycloud_aclient.get_experiment(exp_id)['result']['experiments'][0]
+	return jsonify(exp)
+
+@app.route('/experiment/<exp_id>/active', methods=['GET'])
+@auth.login_required
+def view_active(exp_id):
+	sublist = psycloud_aclient.get_participant_list(exp_id)['result']['participants']
+	active = []
+	for sub in sublist:
+		if sub['status'] == 'ACTIVE':
+			active.append(sub)
+	return render_template('active_dash.html', subs=active)
+
+@app.route('/experiment/<exp_id>/completed', methods=['GET'])
+@auth.login_required
+def view_completed(exp_id):
+	sublist = psycloud_aclient.get_participant_list(exp_id)['result']['participants']
+	completed = []
+	for sub in sublist:
+		if sub['status'] == 'COMPLETED':
+			completed.append(sub)
+	return render_template('completed_dash.html', subs=completed)
+
+@app.route('/participant/<uid>', methods=['GET'])
+@auth.login_required
+def view_participant(uid):
+	sub = psycloud_client.get_participant(uid)['result']['participant']
+	stim_list = psycloud_client.get_stimuli_list(uid)['result']['stimuli']
+	resp_list = psycloud_client.get_response_list(uid)['result']['responses']
+	sub['stimuli'] = stim_list
+	sub['responses'] = resp_list
+	return jsonify(sub)
+
+
+
+@auth.get_password
+def get_password(username):
+    if username == 'username':
+        return 'password'
+    return None
+ 
+# @auth.error_handler
+# def unauthorized():
+#     return make_response(jsonify( { 'status':403, 'message': 'Unauthorized' } ), 403)
+#     # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
---
+++ dashboard/psycloudclient.py
@@ -0,0 +1,149 @@
+"""Psycloud Python Client"""
+import requests
+import json
+
+admin_endpoint = {}
+admin_endpoint['experiment_upload'] = '/psycloud/admin/api/experiments/upload_all_data'
+admin_endpoint['experiment_data'] = '/psycloud/admin/api/experiments/%s/data'
+admin_endpoint['experiments'] = '/psycloud/admin/api/experiments'
+admin_endpoint['participants'] = '/psycloud/admin/api/experiments/%s/participants'
+admin_endpoint['coupons'] = '/psycloud/admin/api/experiments/%s/coupons'
+
+endpoint = {}
+endpoint['register'] = '/psycloud/api/experiment/%s/register'
+endpoint['participant'] = '/psycloud/api/participants/%s'
+endpoint['stimuli'] = '/psycloud/api/participants/%s/stimuli'
+endpoint['responses'] = '/psycloud/api/participants/%s/responses'
+endpoint['data'] = '/psycloud/api/participants/%s/data'
+
+
+class PsycloudAdminClient():
+	def __init__(self, base_url, username, password):
+		self.base_url = base_url
+		self.username = username
+		self.password = password
+
+	def upload_data(self, data_dict=None, json_filename=None):
+		url = self.base_url + admin_endpoint['experiment_upload']
+		headers = {'content-type': 'application/json'}
+		if data_dict is not None:
+			data = data_dict
+		elif json_filename is not None:
+			f = open(json_filename, 'rb')
+			data = json.load(f)
+			f.close()
+		r = requests.post(url, data=json.dumps(data), headers=headers, auth=(self.username, self.password))
+		return r.json()
+
+	def get_experiment_list(self):
+		url = self.base_url + admin_endpoint['experiments']
+		r = requests.get(url, auth=(self.username, self.password))
+		return r.json()
+
+	def get_experiment(self, experiment_id):
+		url = self.base_url + admin_endpoint['experiments'] + '/%s'%experiment_id
+		r = requests.get(url, auth=(self.username, self.password))
+		return r.json()
+
+	def delete_experiment(self, experiment_id):
+		url = self.base_url + admin_endpoint['experiments'] + '/%s'%experiment_id
+		r = requests.delete(url, auth=(self.username, self.password))
+		return r.json()
+
+	def save_coupons(self, experiment_id,  data_dict=None, json_filename=None):
+		url = self.base_url + admin_endpoint['coupons']%experiment_id
+		headers = {'content-type': 'application/json'}
+		if data_dict is not None:
+			data = data_dict
+		elif json_filename is not None:
+			f = open(json_filename, 'rb')
+			data = json.load(f)
+			f.close()
+		r = requests.post(url, data=json.dumps(data), headers=headers, auth=(self.username, self.password))
+		return r.json()
+
+	def get_coupons(self, experiment_id):
+		url = self.base_url + admin_endpoint['coupons']%experiment_id
+		r = requests.get(url, auth=(self.username, self.password))
+		return r.json()
+
+	def get_participant_list(self, experiment_id, keys_only=False, status=None):
+		url = self.base_url + admin_endpoint['participants']%experiment_id
+		d = {}
+		if keys_only:
+			d.update({'keys_only':True})
+		if status is not None:
+			d.update({'status':status})
+		r = requests.get(url, params=d, auth=(self.username, self.password))
+		return r.json()
+
+
+
+class PsycloudClient():
+	def __init__(self, base_url):
+		self.base_url = base_url
+
+	def register(self, experiment_id, registration_coupon=None):
+		url = self.base_url + endpoint['register']%experiment_id
+		if registration_coupon is not None:
+			url = url + '/%s'%registration_coupon
+		r = requests.post(url)
+		return r.json()
+
+	def get_participant(self, participant_id):
+		url = self.base_url + endpoint['participant']%participant_id
+		r = requests.get(url)
+		return r.json()
+
+	def get_stimuli_list(self, participant_id):
+		url = self.base_url + endpoint['stimuli']%participant_id
+		r = requests.get(url)
+		return r.json()
+
+	def get_current_stimulus(self, participant_id):
+		url = self.base_url + endpoint['stimuli']%participant_id + '/current'
+		r = requests.get(url)
+		return r.json()
+
+	def get_stimulus(self, participant_id, stimulus_number):
+		url = self.base_url + endpoint['stimuli']%participant_id + '/%s'%stimulus_number
+		r = requests.get(url)
+		return r.json()
+
+	def increment_and_get_next_stimulus(self, participant_id):
+		url = self.base_url + endpoint['stimuli']%participant_id + '/next'
+		r = requests.put(url)
+		return r.json()
+
+	def save_current_response(self, participant_id, data_dict):
+		url = self.base_url + endpoint['responses']%participant_id + '/current'
+		headers = {'content-type': 'application/json'}
+		r = requests.post(url, data=json.dumps(data_dict), headers=headers)
+		return r.json()
+
+	def save_response(self, participant_id, stimulus_number, data_dict):
+		url = self.base_url + endpoint['responses']%participant_id + '/%s'%stimulus_number
+		headers = {'content-type': 'application/json'}
+		r = requests.post(url, data=json.dumps(data_dict), headers=headers)
+		return r.json()
+
+	def get_response_list(self, participant_id):
+		url = self.base_url + endpoint['responses']%participant_id
+		r = requests.get(url)
+		return r.json()
+
+	def get_previous_response(self, participant_id):
+		url = self.base_url + endpoint['responses']%participant_id + '/previous'
+		r = requests.get(url)
+		return r.json()
+
+	def get_response(self, participant_id, stimulus_number):
+		url = self.base_url + endpoint['responses']%participant_id + '/%s'%stimulus_number
+		r = requests.get(url)
+		return r.json()
+
+	def completed(self, participant_id):
+		url = self.base_url + endpoint['participant']%participant_id + '/completed'
+		r = requests.post(url)
+		return r.json()
+
