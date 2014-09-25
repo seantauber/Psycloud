f11fb024f621f3c87729f9244d8dc3402862ba53
Sean Tauber <seanetauber@gmail.com>
2014-09-25 11:10:44 +0930

    moved admin dashboard into main psycloud server rest api

--- REST-API/main.py
+++ REST-API/main.py
@@ -173,6 +173,82 @@ def get_coupons(experiment_id):
 	else:
 		abort(404)
 
+
+#######################################################################################
+#######################################################################################
+# 
+# ADMINISTRATOR DASHBOARD 
+# 
+#######################################################################################
+#######################################################################################
+
+from psycloudclient import PsycloudClient, PsycloudAdminClient
+psycloud_client = PsycloudClient("http://psycloud-server-1.appspot.com")
+psycloud_aclient = PsycloudAdminClient("http://psycloud-server-1.appspot.com", "username", "password")
+
+
+@app.route('/psycloud/admin/dashboard', methods=['GET'])
+@auth.login_required
+def main_dashboard():
+	exp_list = psycloud_aclient.get_experiment_list()['result']['experiments']
+	exps = []
+	for exp in exp_list:
+		exps.append({'name':exp['experiment_name'], 'id':exp['id'],
+			'num_available':len(exp['available_participants']),
+			'num_active':len(exp['active_participants']),
+			'num_completed':len(exp['completed_participants']),
+			'num_participants':exp['num_participants']})
+	return render_template('main_dashboard.html', params=exps)
+
+@app.route('/psycloud/admin/dashboard/experiment/<exp_id>', methods=['GET'])
+@auth.login_required
+def view_experiment(exp_id):
+	exp = psycloud_aclient.get_experiment(exp_id)['result']['experiments'][0]
+	return jsonify(exp)
+
+@app.route('/psycloud/admin/dashboard/experiment/<exp_id>/active', methods=['GET'])
+@auth.login_required
+def view_active(exp_id):
+	sublist = psycloud_aclient.get_participant_list(exp_id)['result']['participants']
+	active = []
+	for sub in sublist:
+		if sub['status'] == 'ACTIVE':
+			active.append(sub)
+	return render_template('active_dash.html', subs=active)
+
+@app.route('/psycloud/admin/dashboard/experiment/<exp_id>/completed', methods=['GET'])
+@auth.login_required
+def view_completed(exp_id):
+	sublist = psycloud_aclient.get_participant_list(exp_id)['result']['participants']
+	completed = []
+	for sub in sublist:
+		if sub['status'] == 'COMPLETED':
+			completed.append(sub)
+	return render_template('completed_dash.html', subs=completed, exp_id=exp_id)
+
+@app.route('/psycloud/admin/dashboard/participant/<uid>', methods=['GET'])
+@auth.login_required
+def view_participant(uid):
+	sub = psycloud_client.get_participant(uid)['result']['participant']
+	stim_list = psycloud_client.get_stimuli_list(uid)['result']['stimuli']
+	resp_list = psycloud_client.get_response_list(uid)['result']['responses']
+	sub['stimuli'] = stim_list
+	sub['responses'] = resp_list
+	return jsonify(sub)
+
+@app.route('/psycloud/admin/dashboard/experiment/<exp_id>/completed/download_data', methods=['GET'])
+@auth.login_required
+def download_completed_participant_data(exp_id):
+	sublist = psycloud_aclient.get_experiment_data(exp_id, status='COMPLETED')['result']
+	return jsonify(sublist)
+
+
+
+
+
+
+
+
 #######################################################################################
 #######################################################################################
 # 
