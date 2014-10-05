bca200ea1266e69e2d0dc4259de5e055fd963fd5
Sean Tauber <seanetauber@gmail.com>
2014-10-05 13:12:05 +1030

    added datastore methods to set and get current stimulus number of a participant

--- psycloud-server/experiment_datastore.py
+++ psycloud-server/experiment_datastore.py
@@ -344,6 +344,34 @@ class ExperimentDatastoreGoogleNDB():
 		if participant_key is None:
 			raise LookupError('Participant not found.')
 		return participant_key.get().max_number_stimuli
+
+
+	def get_current_stimulus(self, participant_short_id):
+		'''
+		Returns the current stimulus number for the participant.
+		'''
+
+		# Check if the participant exists
+		participant_key = self.lookup_participant(participant_short_id)
+		if participant_key is None:
+			raise LookupError('Participant not found.')
+		return participant_key.get().current_stimulus
+
+	def set_current_stimulus(self, participant_short_id, current_stimulus):
+		'''
+		Sets the current stimulus number for the participant.
+		'''
+
+		# Check if the participant exists
+		participant_key = self.lookup_participant(participant_short_id)
+		if participant_key is None:
+			raise LookupError('Participant not found.')
+		
+		participant = participant_key.get()
+
+		participant.current_stimulus = current_stimulus
+		participant.put()
+		return True
 		
 
 	def get_stimuli(self, participant_short_id, stimulus_number=None):
