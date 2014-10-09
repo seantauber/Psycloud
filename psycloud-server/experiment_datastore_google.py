66a52c2f1ef870b50403327d6754ea65f25a362b
Sean Tauber <seanetauber@gmail.com>
2014-10-09 12:08:40 +1030

    modified file structure for datastore

--- psycloud-server/experiment_datastore_google.py
+++ psycloud-server/experiment_datastore_google.py
@@ -3,12 +3,53 @@ from datetime import datetime
 from uuid import uuid4
 from base64 import urlsafe_b64encode
 
-from ndb_models import Experiment, Participant, Stimulus, Response, RegistrationCoupon
 from custom_exceptions import DuplicateEntryError, ResourceError, DataFormatError
 
 
-class AdminDatastore():
+class Experiment(ndb.Model):
+	creation_time = ndb.DateTimeProperty(auto_now_add=True)
+	short_id = ndb.StringProperty()
+	experiment_name = ndb.StringProperty()
+	num_participants = ndb.IntegerProperty()
+	available_participants = ndb.JsonProperty()
+	active_participants = ndb.JsonProperty()
+	completed_participants = ndb.JsonProperty()
+	stalled_participants = ndb.JsonProperty()
 
+class Participant(ndb.Model):
+	creation_time = ndb.DateTimeProperty(auto_now_add=True)
+	short_id = ndb.StringProperty()
+	conf_code = ndb.StringProperty()
+	participant_index = ndb.IntegerProperty()
+	status = ndb.StringProperty()
+	start_time = ndb.DateTimeProperty()
+	end_time = ndb.DateTimeProperty()	
+	stimuli_count = ndb.IntegerProperty()
+	max_number_stimuli = ndb.IntegerProperty()
+	current_stimulus = ndb.IntegerProperty()
+	registration_coupon = ndb.JsonProperty()
+	details = ndb.JsonProperty()
+
+class Stimulus(ndb.Model):
+	creation_time = ndb.DateTimeProperty(auto_now_add=True)
+	stimulus_index = ndb.IntegerProperty()
+	variables = ndb.JsonProperty()
+	stimulus_type = ndb.StringProperty()
+
+class Response(ndb.Model):
+	creation_time = ndb.DateTimeProperty(auto_now_add=True)
+	stimulus_index = ndb.IntegerProperty()
+	variables = ndb.JsonProperty()
+
+class RegistrationCoupon(ndb.Model):
+	creation_time = ndb.DateTimeProperty(auto_now_add=True)
+	coupon_type = ndb.StringProperty()
+	coupon_value = ndb.StringProperty()
+
+
+
+
+class AdminDatastore():
 
 	def __init__(self):
 		pass
@@ -627,7 +668,6 @@ class ClientDatastoreUtilityMixin():
 		# Create a new coupon for the experiment and save
 		coupon = RegistrationCoupon(parent=experiment_key, coupon_value=coupon)
 		coupon.put()
-
 
 
 
